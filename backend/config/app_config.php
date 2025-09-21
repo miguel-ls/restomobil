3a8cebb3c2f2ffae67138ed8eed89411b616348a
Miguel Lopez <miguel.lopez@codesicorp.com>
2025-09-21 01:06:03 -0500

    localhost

--- backend/config/app_config.php
+++ backend/config/app_config.php
@@ -10,7 +10,7 @@
 define('CURRENCY_SYMBOL', 'S/.');
 
 // Define la URL base para la API. Asegúrate de que termine con una barra inclinada (/).
-define('API_BASE_URL', 'http://localhost/restaurante_system/backend/api/v1/');
+define('API_BASE_URL', 'http://localhost/personal/restaurante_system/backend/api/v1/');
 
 // Aquí se pueden añadir otras configuraciones globales en el futuro.
 ?>
